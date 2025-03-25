5e354b2dbf2a2701db74096ab73d85099877f617
iuri19051996 <iuri.floriano@vitru.com.br>
2025-03-24 23:39:36 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function enviarWhatsapp() {
   let nomeIndicado = document.getElementById("nome_indicado").value;
-  let telefoneIndicado = document.getElementById("telefone_indicado").value;
+  let telefoneIndicado = document.getElementById("telefone_indicado").value.replace(/\D/g, ''); // Remove caracteres não numéricos
   let emailIndicado = document.getElementById("email_indicado").value;
   let seuNome = document.getElementById("seu_nome").value;
 
-  if (!nomeIndicado || !telefoneIndicado || !seuNome) {
-      alert("Por favor, preencha todos os campos obrigatórios.");
+  if (!nomeIndicado || !telefoneIndicado || telefoneIndicado.length !== 11 || !seuNome) {
+      alert("Por favor, preencha todos os campos obrigatórios corretamente.");
       return;
   }
 
@@ -18,23 +18,8 @@ function enviarWhatsapp() {
   `*Indicado por:* ${seuNome}`;
 
   let encodedMessage = encodeURIComponent(mensagem);
-
-  // Detecta se é mobile
-  let isMobile = /Android|iPhone|iPad/i.test(navigator.userAgent);
   
-  let url;
-  if (isMobile) {
-      if (/Android/i.test(navigator.userAgent)) {
-          // Força a abertura no WhatsApp no Android
-          url = `intent://send/?text=${encodedMessage}#Intent;scheme=whatsapp;package=com.whatsapp;end;`;
-      } else {
-          // Força a abertura no WhatsApp no iPhone
-          url = `whatsapp://send?text=${encodedMessage}`;
-      }
-  } else {
-      // Para desktop, abre no WhatsApp Web
-      url = `https://web.whatsapp.com/send?text=${encodedMessage}`;
-  }
+  let url = `https://api.whatsapp.com/send?text=${encodedMessage}`;
 
   window.location.href = url;
 }
@@ -47,7 +32,7 @@ document.getElementById('telefone_indicado').addEventListener('input', function 
 
   let formatted = input;
   if (input.length > 2) formatted = `(${input.slice(0, 2)}) ${input.slice(2)}`;
-  if (input.length > 6) formatted = `(${input.slice(0, 2)}) ${input.slice(2, 7)}-${input.slice(7)}`;
+  if (input.length > 6) formatted = `(${input.slice(0, 2)}) ${input.slice(2, 7)}${input.slice(7)}`;
 
   e.target.value = formatted;
 });
